eee982adc61f81d75598857079a8861110d7be3a
cnavajas <c.navajas4@gmail.com>
2024-11-10 11:51:53 +0100

    More vim remaps for advance motions: - yank and paste - move selected section up and down

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,8 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.api.nvim_exec('language en_US', true)
 
+vim.o.foldmethod = indent
+
 -- Set leader key
 vim.g.mapleader = " "
 
@@ -19,6 +21,14 @@ vim.api.nvim_set_keymap('n', '<C-p>', ':GFiles<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>pf', ':Files<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-k>', ':cnext<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-j>', ':cprev<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', '<leader>p', '"_dP', { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>y', '"*y', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>y', '"*y', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>Y', 'magg"*yG`a', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>\'', '<C-^>', { noremap = true })
 
