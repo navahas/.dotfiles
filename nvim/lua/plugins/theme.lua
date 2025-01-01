4ff739f4ef20e3c915a72b16c0f8f68243207f67
cnavajas <c.navajas4@gmail.com>
2025-01-01 20:30:02 +0100

    Update hyprland config inputs to behave as macos

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,13 +1,13 @@
 return {
     -- Theme: vim-code-dark
-    {
-        "tomasiser/vim-code-dark",
-        lazy = false, -- Load immediately since it's a theme
-        priority = 1000, -- Ensure it loads first
-        config = function()
-            vim.cmd([[colorscheme codedark]])
-        end,
-    },
+    -- {
+    --     "tomasiser/vim-code-dark",
+    --     lazy = false, -- Load immediately since it's a theme
+    --     priority = 1000, -- Ensure it loads first
+    --     config = function()
+    --         vim.cmd([[colorscheme codedark]])
+    --     end,
+    -- },
 
     -- Theme: vscode.nvim
     {
