58134d2aa98b8f7a88dc57b09070bf799ff47841
Carlos Navajas <c.navajas4@gmail.com>
2025-01-21 17:17:00 +0100

    feat(scripts): tmux-fzf delete multiple sessions

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -26,3 +26,12 @@ map('n', '<leader>Y', 'magg"*yG`a', opts)
 -- Misc key mappings
 map('n', '<leader>nh', ':nohl<CR>', opts)
 map('n', '<leader>\'', '<C-^>', opts)
+
+-- Wrapped lines
+vim.keymap.set({'n', 'v'}, 'j', function()
+  return vim.v.count == 0 and 'gj' or 'j'
+end, {expr = true})
+
+vim.keymap.set({'n', 'v'}, 'k', function()
+  return vim.v.count == 0 and 'gk' or 'k'
+end, {expr = true})
--- scripts/tmux-fzf-sessions.sh
+++ scripts/tmux-fzf-sessions.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-selected_session=$(tmux list-sessions -F "#{session_name}" | \
-   grep -v "^_manager$" | \
-   fzf --preview 'tmux capture-pane -e -t {}:$(tmux list-windows -t {} -F "#{window_active} #{window_index}" | grep "^1" | cut -d" " -f2) -p' \
-   --preview-window=right:50%)
+manager_session="_manager"
+selected_sessions=$(tmux list-sessions -F "#{session_name}" | \
+    grep -v "^$manager_session$" | \
+    fzf -m --preview 'tmux capture-pane -e -t {}:$(tmux list-windows -t {} -F "#{window_active} #{window_index}" | grep "^1" | cut -d" " -f2) -p')
 
-if [ -z "$selected_session" ]; then
-    exit 0
-fi
+[ -z "$selected_sessions" ] && exit 0
 
-if [ -z "$TMUX" ]; then
-    tmux attach-session -t "$selected_session"
+if [ $(echo "$selected_sessions" | wc -l) -gt 1 ]; then
+    echo "$selected_sessions" | while read -r session; do
+        tmux kill-session -t "$session"
+        tmux list-windows -t "$manager_session" -F "#{window_name}" | grep -q "^$session$" && \
+            tmux kill-window -t "$manager_session:$session"
+    done
 else
-    tmux switch-client -t "$selected_session"
+    [ -z "$TMUX" ] && tmux attach-session -t "$selected_sessions" || tmux switch-client -t "$selected_sessions"
 fi
