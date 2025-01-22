0f91cd7a9f8dafea29ce2fee37e31a09b0737b3c
cnavajas <c.navajas4@gmail.com>
2025-01-22 21:01:40 +0100

    fix(nvim): refactor rounded window for treesitter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,6 +21,21 @@ return {
                 border = "rounded",
             },
         })
+        -- Ensure rounded borders for hover, signature help, and diagnostics
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+            vim.lsp.handlers.hover,
+            { border = "rounded" }
+        )
+
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help,
+            { border = "rounded" }
+        )
+
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics,
+            { float = { border = "rounded" } }
+        )
 
         -- Setup Mason and Mason-LSPConfig
         require("mason").setup()
