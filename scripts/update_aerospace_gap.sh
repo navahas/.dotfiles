0a61165e79270f564d50a1e9bb29996c78cf3ec0
Carlos Navajas <c.navajas4@gmail.com>
2025-01-26 08:23:05 +0100

    fx(aerospace): main file created to avoid git conflict

--- scripts/update_aerospace_gap.sh
+++ scripts/update_aerospace_gap.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 aerospace_file="$HOME/.dotfiles/aerospace/aerospace.toml"
 
-if [[ "$(whoami)" == "cnavajas" ]]; then
-    sed -i '' '37,43c\
+if [[ "$(whoami)" != "cnavajas" ]]; then
+    sed -i '' '37i\
 [gaps]\
 inner.horizontal = 11\
 inner.vertical = 11\
 outer.left = 11\
 outer.bottom = 11\
 outer.top = 11\
-outer.right = 11' "$aerospace_file"
+outer.right = 11\
+\
+' "$aerospace_file"
 else
-    sed -i '' '37,43c\
+    sed -i '' '37i\
 [gaps]\
 inner.horizontal = 24\
 inner.vertical = 24\
 outer.left = 24\
 outer.bottom = 24\
 outer.top = 24\
-outer.right = 24' "$aerospace_file"
+outer.right = 24\
+\
+' "$aerospace_file"
 fi
 exit 0
