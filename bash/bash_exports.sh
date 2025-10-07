1ae52d1f676129aa08c4fe5588d1ce303ec55248
Carlos Navajas <c.navajas4@gmail.com>
2025-10-07 16:03:04 +0200

    fix(bash): nvm.sh sourced

--- bash/bash_exports.sh
+++ bash/bash_exports.sh
@@ -9,6 +9,7 @@ export VISUAL=nvim
 # Tool configuration
 export HOMEBREW_NO_AUTO_UPDATE=1
 export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 export EZA_CONFIG_DIR="$HOME/.config/eza"
 export BAT_THEME="Visual Studio Dark+"
 export FZF_DEFAULT_OPTS='--reverse'
