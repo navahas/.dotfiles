3a15dff464c746f58005c1148b76abda90b9bd94
Carlos Navajas <navahas@culturecode.dev>
2025-10-30 11:37:26 +0100

    fix(nvim): add all modes in custom ui status bar

--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -48,14 +48,60 @@ local mode_display_enabled = true
 
 vim.opt.showmode = false
 
+local style_operator = "——>"
 local mode_map = {
+    -- Normal modes
     n = " ",
-    i = " ——> INSERT",
-    v = " ——> VISUAL",
-    V = " ——> VISUAL LINE",
-    ["\22"] = " ——> VISUAL BLOCK",
-    s = " ——> SELECT",
-    t = " ——> TERMINAL",
+    no = " " .. style_operator .. " OPERATOR-PENDING",
+    nov = " " .. style_operator .. " OPERATOR-PENDING (charwise)",
+    noV = " " .. style_operator .. " OPERATOR-PENDING (linewise)",
+    ["\22"] = " " .. style_operator .. " VISUAL BLOCK",  -- CTRL-V
+    ["no\22"] = " " .. style_operator .. " OPERATOR-PENDING (blockwise)",
+    niI = " " .. style_operator .. " NORMAL (insert)",
+    niR = " " .. style_operator .. " NORMAL (replace)",
+    niV = " " .. style_operator .. " NORMAL (virtual replace)",
+    nt = " " .. style_operator .. " NORMAL (terminal)",
+    ntT = " " .. style_operator .. " NORMAL (terminal ctrl-o)",
+
+    -- Visual modes
+    v = " " .. style_operator .. " VISUAL",
+    vs = " " .. style_operator .. " VISUAL (select)",
+    V = " " .. style_operator .. " VISUAL LINE",
+    Vs = " " .. style_operator .. " VISUAL LINE (select)",
+    ["\22s"] = " " .. style_operator .. " VISUAL BLOCK (select)",  -- CTRL-Vs
+
+    -- Select modes
+    s = " " .. style_operator .. " SELECT",
+    S = " " .. style_operator .. " SELECT LINE",
+    ["\19"] = " " .. style_operator .. " SELECT BLOCK",  -- CTRL-S
+
+    -- Insert modes
+    i = " " .. style_operator .. " INSERT",
+    ic = " " .. style_operator .. " INSERT (completion)",
+    ix = " " .. style_operator .. " INSERT (ctrl-x completion)",
+
+    -- Replace modes
+    R = " " .. style_operator .. " REPLACE",
+    Rc = " " .. style_operator .. " REPLACE (completion)",
+    Rx = " " .. style_operator .. " REPLACE (ctrl-x completion)",
+    Rv = " " .. style_operator .. " VIRTUAL REPLACE",
+    Rvc = " " .. style_operator .. " VIRTUAL REPLACE (completion)",
+    Rvx = " " .. style_operator .. " VIRTUAL REPLACE (ctrl-x completion)",
+
+    -- Command-line modes
+    -- c = " " .. style_operator .. " COMMAND",
+    -- cr = " " .. style_operator .. " COMMAND (overstrike)",
+    cv = " " .. style_operator .. " EX",
+    cvr = " " .. style_operator .. " EX (overstrike)",
+
+    -- Prompt modes
+    r = " " .. style_operator .. " HIT-ENTER",
+    rm = " " .. style_operator .. " MORE",
+    ["r?"] = " " .. style_operator .. " CONFIRM",
+
+    -- Other
+    ["!"] = " " .. style_operator .. " SHELL",
+    t = " " .. style_operator .. " TERMINAL",
 }
 
 -- Toggle function for mode display
