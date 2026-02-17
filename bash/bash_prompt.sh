92b92a61471f5e044ff4c8a3a7456d7edb554132
Carlos Navajas <navahas@culturecode.dev>
2026-02-17 21:53:19 +0100

    feat(bash): update prompt to identify nix develop and bash shell

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -7,6 +7,7 @@ COLOR_GREEN='\[\e[32m\]'
 COLOR_RED='\[\e[31m\]'
 COLOR_GREY='\[\e[38;5;244m\]'
 COLOR_RESET='\[\e[0m\]'
+COLOR_LIGHT_BLUE='\[\e[94m\]'
 
 # Function to get git branch and dirty status
 __git_prompt() {
@@ -38,18 +39,30 @@ __status_prompt() {
 __build_prompt() {
     local last_status=$?
 
-    # Current directory (replace $HOME with ~)
-    local current_dir="$PWD"
-    if [[ "$PWD" == "$HOME"* ]]; then
-        current_dir="~${PWD#$HOME}"
+    # Shorten path to last 2 dirs [dir1::dir2]
+    local path="${PWD/#$HOME/HOME}"
+    IFS='/' read -ra parts <<< "$path"
+    local count=${#parts[@]}
+    local show
+    if [ $count -le 1 ]; then
+        show="[$path]"
+    else
+        show="[${parts[-2]}::${parts[-1]}]"
+    fi
+
+    # Nix indicator
+    local nix_part=""
+    if [ -n "$IN_NIX_SHELL" ] || [ -n "$NIX_STORE" ]; then
+        nix_part="${COLOR_LIGHT_BLUE}[NIX]${COLOR_RESET}"
     fi
 
     # Build prompt components
-    local dir_part="${COLOR_BLUE}${current_dir}${COLOR_RESET}"
+    local dir_part="${COLOR_BLUE}${show}${COLOR_RESET}"
+    local bash_part="${COLOR_GREEN}[$]${COLOR_RESET}"
     local git_part="$(__git_prompt)"
     local status_part="$(__status_prompt $last_status)"
 
-    PS1=" ${dir_part} ${git_part}${status_part}"
+    PS1="${nix_part}${dir_part}${bash_part} ${git_part}${status_part}"
 }
 
 # Use PROMPT_COMMAND to set prompt dynamically
