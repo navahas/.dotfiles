30848645207b8c152be9b55bf2b8da2034292895
Carlos Navajas <navahas@culturecode.dev>
2026-02-18 06:22:16 +0100

    fix(fish): fman command to use bat as pager

--- fish/functions/fman.fish
+++ fish/functions/fman.fish
@@ -1,6 +1,8 @@
 function fman
     set query (rg --files $HOME/man/share/man | sed "s|$HOME/man/share/man/||" | fzf)
     if test -n "$query"
-        man -l "$HOME/man/share/man/$query" | col -bx | bat --style=plain --language=man --paging=always
+        GROFF_NO_SGR=1 man -l "$HOME/man/share/man/$query" | \
+        col -bx | \
+        bat --theme=OneHalfDark --style=plain --language=man --paging=always
     end
 end
