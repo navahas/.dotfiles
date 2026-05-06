025f41c8f4648399fa55dba7bb6f207ebd7ab8e2
navahas <navahas@culturecode.dev>
2026-05-06 18:15:10 +0000

    feat(nvim/v0.12.0): remove treesitter to use built-in

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,7 +4,6 @@ vim.pack.add({
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/nvim-telescope/telescope.nvim" },
     { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/wtfox/jellybeans.nvim" },
     { src = "https://github.com/williamboman/mason.nvim" },
     { src = "https://github.com/williamboman/mason-lspconfig.nvim" },
@@ -22,7 +21,6 @@ vim.cmd.packadd('plenary.nvim')
 vim.cmd.packadd('nvim-web-devicons')
 vim.cmd.packadd('telescope.nvim')
 vim.cmd.packadd('telescope-fzf-native.nvim')
-vim.cmd.packadd('nvim-treesitter')
 vim.cmd.packadd('jellybeans.nvim')
 vim.cmd.packadd('mason.nvim')
 vim.cmd.packadd('mason-lspconfig.nvim')
