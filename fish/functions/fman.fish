a43cab7c608e821853a54e8fec7380eae86a0a19
Carlos Navajas <navahas@culturecode.dev>
2026-08-23 11:07:34 +0200

    fix(man): fit local pages to terminal width and clean up setup-man find

--- fish/functions/fman.fish
+++ fish/functions/fman.fish
@@ -1,7 +1,7 @@
 function fman
     set query (rg --files $HOME/man/share/man | sed "s|$HOME/man/share/man/||" | fzf)
     if test -n "$query"
-        man -l "$HOME/man/share/man/$query" | \
+        env MANWIDTH=$COLUMNS man -l "$HOME/man/share/man/$query" 2>/dev/null | \
             col -bx | \
             bat --style=plain --language=man --paging=always
     end
